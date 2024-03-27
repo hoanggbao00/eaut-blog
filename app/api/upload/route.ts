e5a305b6476146bbaeb385c8c437663d103899a8
hoanggbao <kaitou.kurosaki@gmail.com>
2024-03-27 20:32:45 +0700

    fix route

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,22 +1,15 @@
 import { put } from "@vercel/blob";
 import { NextResponse } from "next/server";
 
-export async function POST(
-  request: ReadableStream,
-): Promise<NextResponse> {
+export async function POST(request: Request): Promise<NextResponse> {
+  const { searchParams } = new URL(request.url);
+  const filename = searchParams.get("filename") || "filename";
 
-  try {
-  const blob = await put('thumbnail', request, {
-    access: 'public',
-  })
-    if (!blob) return NextResponse.json({ message: "Failed" }, { status: 401 });
+  if (!request.body)
+    return NextResponse.json({ message: "Failed" }, { status: 404 });
+  const blob = await put(filename, request.body, {
+    access: "public",
+  });
 
-    return NextResponse.json(blob);
-  } catch (error) {
-    console.log(error);
-    return NextResponse.json(
-      { message: "Something went wrong" },
-      { status: 500 },
-    );
-  }
+  return NextResponse.json(blob);
 }
